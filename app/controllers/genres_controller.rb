8c8b7a92200d569a06e4580882a40a22fc9fa47e
YuNakazawa1244 <g.o.w.yu32190331153@gmail.com>
2021-05-11 07:15:22 +0000

    [create]genre_model

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,22 @@
+class GenresController < ApplicationController
+  def new
+  end
+  
+  def create
+  end
+  
+  def index
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+  
+  def update
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_064346) do
+ActiveRecord::Schema.define(version: 2021_05_11_065800) do
 
   create_table "dishes", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2021_05_11_064346) do
     t.text "source"
     t.text "memo"
     t.float "satisfaction"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
