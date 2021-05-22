64245f4264496e734d48f428e5b2373643b086da
Yunakazawa1244 <g.o.w.yu32190331153@gmail.com>
2021-05-22 12:37:38 +0000

    [Add]TOP_header,footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   devise_for :users
   root to: "homes#top"
+  resources :users, only:[:index,:show,:edit,:update,:destroy]
   resources :dishes
   resources :genres
 end
